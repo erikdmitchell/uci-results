0918175974c0cff47b2bc8d89b375cf054091910
erikdmitchell <erikdmitchell@gmail.com>
2015-11-19 09:36:42 -0500

    working on rider search

--- classes/view-db.php
+++ classes/view-db.php
@@ -17,6 +17,7 @@ class ViewDB {
 		add_action('admin_enqueue_scripts',array($this,'viewdb_scripts_styles'));
 		add_action('wp_ajax_race_search',array($this,'ajax_race_search'));
 		add_action('wp_ajax_race_filter',array($this,'ajax_race_filter'));
+		add_action('wp_ajax_rider_search',array($this,'ajax_rider_search'));
 
 		$this->url=admin_url('admin.php?page=uci-view-db');
 	}
@@ -102,29 +103,90 @@ class ViewDB {
 							</form>
 							<div class="row">
 								<div class="col-md-12">
-									<h4>Search</h4>
+									<h4>Search by Name</h4>
 								</div>
 								<div class="race-search col-md-6">
 									<input id="race-search" type="text" />
 								</div>
-								<div class="race-search col-md-6">
+								<div class="col-md-6">
 									<button type="reset" id="clear-race-search" value="Clear">Clear</button>
 								</div>
 							</div><!-- .row -->
 							<div class="row">
 								<div class="race-search-results col-md-12">
-									<div id="race-search-results-text">Search Results...</div>
+									<div id="race-search-results-text">Search Races...</div>
 								</div>
 							</div><!-- .row -->
 						</div><!-- .filters -->
 					</div><!-- .races -->
-					<div class="races col-md-6">
+					<div class="riders col-md-6">
 						<h3>Riders</h3>
+						<div class="filters">
+							<form name="rider_filters" id="rider_filters">
+								<div class="row">
+									<div class="season col-md-4">
+										<h4>Season</h4>
+										<select name="season" class="season">
+											<option value="0">-- Select One --</option>
+											<?php foreach ($seasons as $season) : ?>
+												<option value="<?php echo $season; ?>"><?php echo $season; ?></option>
+											<?php endforeach; ?>
+										</select>
+									</div>
+									<div class="class col-md-4">
+										<h4>Class</h4>
+										<select name="class" class="class">
+											<option value="0">-- Select One --</option>
+											<?php foreach ($classes as $class) : ?>
+												<option value="<?php echo $class; ?>"><?php echo $class; ?></option>
+											<?php endforeach; ?>
+										</select>
+									</div>
+									<div class="country col-md-4">
+										<h4>Country</h4>
+										<select name="nat" class="nat">
+											<option value="0">-- Select One --</option>
+											<?php foreach ($countries as $country) : ?>
+												<option value="<?php echo $country; ?>"><?php echo $country; ?></option>
+											<?php endforeach; ?>
+										</select>
+									</div>
+								</div><!-- .row -->
+
+								<div class="row">
+									<div class="race-search-buttons col-md-12">
+										<p>
+											<button type="reset" id="form-reset" value="Reset">Reset</button>
+										</p>
+									</div>
+								</div><!-- .row -->
+
+							</form>
+							<div class="row">
+								<div class="col-md-12">
+									<h4>Search by Name</h4>
+								</div>
+								<div class="rider-search col-md-6">
+									<input id="rider-search" type="text" />
+								</div>
+								<div class="col-md-6">
+									<button type="reset" id="clear-rider-search" value="Clear">Clear</button>
+								</div>
+							</div><!-- .row -->
+							<div class="row">
+								<div class="rider-search-results col-md-12">
+									<div id="rider-search-results-text">Search Riders...</div>
+								</div>
+							</div><!-- .row -->
+						</div><!-- .filters -->
 					</div><!-- .riders -->
 				</div>
 				<div class="row data">
 					<?php if (isset($_GET['race_code'])) : ?>
 						<?php echo $this->get_race_data($_GET['race_code']); ?>
+					<?php endif; ?>
+					<?php if (isset($_GET['rider'])) : ?>
+						<?php echo $this->get_rider_data($_GET['rider']); ?>
 					<?php endif; ?>
 				</div>
 			</div>
@@ -216,6 +278,76 @@ print_r($_POST);
 echo '</pre>';
 	}
 
+	protected function get_rider_data($rider_name) {
+		global $RiderStats;
+
+		$html=null;
+		//$rider=$RiderStats->get_riders(array('name' => $rider_name));
+		//$race=$RaceStats->get_race($race_code);
+		//$race_classes=$RaceStats->get_race_classes();
+		$CrossSeasons=new CrossSeasons();
+		$counter=0;
+
+		$html.='<div class="view-db-single-rider col-md-12">';
+
+			$html.='<h4>'.$race->details->race.'</h4>';
+
+			$html.='<form name="edit-race" id="edit-race" method="post" action="'.$this->url.'">';
+				$html.='<div class="row header">';
+					$html.='<div class="date col-md-2">Date</div>';
+					$html.='<div class="class col-md-2">Class</div>';
+					$html.='<div class="nat col-md-2">Nat</div>';
+					$html.='<div class="season col-md-2">Season</div>';
+				$html.='</div>';
+				$html.='<div class="row race-details">';
+					$html.='<div class="date col-md-2"><input name="race[date]" id="race-date" value="'.$race->details->date.'" /></div>';
+					$html.='<div class="class col-md-2">';
+						$html.='<select name="race[class]" id="race-class">';
+							foreach ($race_classes as $class) :
+								$html.='<option value="'.$class.'" '.selected($race->details->class,$class,false).'>'.$class.'</option>';
+							endforeach;
+						$html.='</select>';
+					$html.='</div>';
+					$html.='<div class="nat col-md-2"><input name="race[date]" id="race-date" value="'.$race->details->nat.'" /></div>';
+					$html.='<div class="season col-md-2">';
+						$html.='<select name="race[season]" id="race-season">';
+							foreach ($CrossSeasons->seasons as $season) :
+								$html.='<option value="'.$season.'" '.selected($race->details->season,$season,false).'>'.$season.'</option>';
+							endforeach;
+						$html.='</select>';
+					$html.='</div>';
+				$html.='</div>';
+				$html.='<div class="row header">';
+					$html.='<div class="place col-md-1">Place</div>';
+					$html.='<div class="rider col-md-3">Rider</div>';
+					$html.='<div class="nat col-md-1">Nat</div>';
+					$html.='<div class="age col-md-1">Age</div>';
+					$html.='<div class="time col-md-1">Time</div>';
+					$html.='<div class="points col-md-1">Points</div>';
+				$html.='</div>';
+				$html.='<div class="results">';
+					foreach ($race->results as $result) :
+						$html.='<div id="rider-'.$counter.'" class="row result">';
+							$html.='<div class="place col-md-1"><input type="text" name="rider['.$counter.'][place]" id="rider-place" value="'.$result->place.'" /></div>';
+							$html.='<div class="rider col-md-3"><input type="text" name="rider['.$counter.'][rider]" id="rider-rider" value="'.$result->rider.'" /></div>';
+							$html.='<div class="nat col-md-1"><input type="text" name="rider['.$counter.'][nat]" id="rider-nat" value="'.$result->nat.'" /></div>';
+							$html.='<div class="age col-md-1"><input type="text" name="rider['.$counter.'][age]" id="rider-age" value="'.$result->age.'" /></div>';
+							$html.='<div class="time col-md-1"><input type="text" name="rider['.$counter.'][time]" id="rider-time" value="'.$result->time.'" /></div>';
+							$html.='<div class="points col-md-1"><input type="text" name="rider['.$counter.'][points]" id="rider-points" value="'.$result->points.'" /></div>';
+						$html.='</div>';
+						$counter++;
+					endforeach;
+				$html.='</div>';
+				$html.='<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>';
+				$html.='<input type="hidden" name="race-code" value="'.$race_code.'" />';
+				$html.='<input type="hidden" name="update-race" value="1" />';
+			$html.='</form>';
+
+		$html.='</div>';
+
+		return $html;
+	}
+
 	/**
 	 * ajax_race_search function.
 	 *
@@ -299,6 +431,46 @@ echo '</pre>';
 					$html.='<div class="nat col-md-1">'.$race->nat.'</div>';
 					$html.='<div class="class col-md-1">'.$race->class.'</div>';
 					$html.='<div class="fq col-md-1">'.$race->fq.'</div>';
+				$html.='</div>';
+			endforeach;
+		$html.='</div>';
+
+		echo $html;
+
+		wp_die();
+	}
+
+	/**
+	 * ajax_rider_search function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function ajax_rider_search() {
+		global $wpdb,$uci_curl;
+
+		$html=null;
+		$sql="
+			SELECT
+				name,
+				nat
+			FROM $uci_curl->results_table
+			WHERE name LIKE '%".$_POST['search']."%'
+			GROUP BY name
+			ORDER BY name
+		";
+		$riders=$wpdb->get_results($sql);
+
+		if (!count($riders)) :
+			echo 'No riders found.';
+			return;
+		endif;
+
+		$html.='<div class="riders">';
+			foreach ($riders as $rider) :
+				$html.='<div id="rider-'.str_replace(' ','',$rider->name).'" class="row rider">';
+					$html.='<div class="name col-md-5"><a href="'.$this->url.'&rider='.urlencode($rider->name).'">'.stripslashes($rider->name).'</a></div>';
+					$html.='<div class="nat col-md-1">'.$rider->nat.'</div>';
 				$html.='</div>';
 			endforeach;
 		$html.='</div>';
--- js/view-db.js
+++ js/view-db.js
@@ -23,14 +23,14 @@ jQuery(document).ready(function($) {
 
 	// race filters //
 	$('#race_filters .season, #race_filters .class, #race_filters .nat').change(function() {
-			var data={
-				'action' : 'race_filter',
-				'form' : $('#race_filters').serialize()
-			};
+		var data={
+			'action' : 'race_filter',
+			'form' : $('#race_filters').serialize()
+		};
 
-			$.post(ajaxurl,data,function(response) {
-				$('.row.data').html(response);
-			});
+		$.post(ajaxurl,data,function(response) {
+			$('.row.data').html(response);
+		});
 	});
 
 	// form reset //
@@ -39,6 +39,27 @@ jQuery(document).ready(function($) {
 
 		$('#race-search').val(''); // clear search box
 		$('#race-search-results-text').html('').hide(); // clear results box and hide
+
+		return false;
+	});
+
+	// rider search //
+	$("input#rider-search").live("keyup", function(e) {
+		// Set Search String
+		var search_string = $(this).val();
+
+		// Do Search
+		if (search_string !== '' && search_string.length>=3) {
+			var data={
+				'action' : 'rider_search',
+				'search' : search_string
+			};
+
+			$.post(ajaxurl,data,function(response) {
+				$('#rider-search-results-text').show();
+				$('#rider-search-results-text').html(response);
+			});
+		}
 
 		return false;
 	});
