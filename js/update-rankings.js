bbc9894999a2077ded7b9aa307593c3add0a136b
erikdmitchell <erikdmitchell@gmail.com>
2016-06-14 15:55:14 -0400

    buit out js for a visual season ranks updater

--- js/update-rankings.js
+++ js/update-rankings.js
@@ -7,16 +7,42 @@ jQuery(document).ready(function($) {
 
 		$modal.show();
 
+		var $btn=$(this);
+		var season=$('.season').val();
 		var data={
-			action : 'update_rider_rankings',
-			season : $('.season').val()
+			action : 'update_rider_rankings_get_rider_ids',
+			season : season
 		};
 
-		$('#update-rider-ranking-notes').append('<div class="note">Calculating rider rankings...</div>');
+		$btn.prop('disabled', true);
 
-		$.post(ajaxurl, data, function(response) {
-			$('#update-rider-ranking-notes').append('<div class="updated">Rider rankings complete.</div>');
+		$.post(ajaxurl, data, function(rider_ids) {
+			var total_riders=rider_ids.length;
+			var rider_counter=0;
+
+			$('.update-rider-ranking-notes .rider-ranking-totals .total').text(total_riders);
+			$('.update-rider-ranking-notes').show();
+
+			$modal.hide();
+
+			// cycle through rider ids and update //
+			for (var i in rider_ids) {
+				var update_data={
+					'action' : 'update_rider_rankings',
+					'rider_id' : rider_ids[i],
+					'season' : season
+				};
+
+				$.post(ajaxurl, update_data, function(response) {
+					rider_counter++;
+
+					$('.update-rider-ranking-notes .rider-ranking-totals .current-count').text(rider_counter);
+					$('.update-rider-ranking-notes .response-result').append(response);
+				});
+			}
 		});
+
+		$btn.prop('disabled', false);
 	});
 
 });
