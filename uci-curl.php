79d3ca299ef107a8f2722280facd30e0566800d4
erikdmitchell <erikdmitchell@gmail.com>
2016-06-17 15:45:40 -0400

    included our new classes

--- uci-curl.php
+++ uci-curl.php
@@ -16,6 +16,11 @@ include_once(UCICURL_PATH.'lib/log.php'); // our log functions
 include_once(UCICURL_PATH.'database.php'); // sets up our db tables
 include_once(UCICURL_PATH.'functions.php'); // all our outlying functions
 include_once(UCICURL_PATH.'classes/admin.php'); // our admin panel and curl functions
+
+include_once(UCICURL_PATH.'admin/admin-pages.php'); // admin page
+include_once(UCICURL_PATH.'admin/add-races.php'); // cURL and add races/results to db
+include_once(UCICURL_PATH.'admin/rider-rankings.php'); // add and update rider rankings
+
 include_once(UCICURL_PATH.'lib/name-parser.php'); // a php nameparser
 include_once(UCICURL_PATH.'classes/races.php'); // our races functions
 include_once(UCICURL_PATH.'classes/riders.php'); // our riders functions
