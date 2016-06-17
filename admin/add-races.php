b29c10e1b83333600beb6490f86eeca92230d1a2
erikdmitchell <erikdmitchell@gmail.com>
2016-06-17 15:29:03 -0400

    moved all add races functionality to class

---
+++ admin/add-races.php
@@ -0,0 +1,546 @@
+<?php
+
+/**
+ * UCIResultsAddRaces class.
+ */
+class UCIResultsAddRaces {
+
+	/**
+	 * __construct function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function __construct() {
+		add_action('wp_ajax_get_race_data_non_db',array($this,'ajax_get_race_data_non_db'));
+		add_action('wp_ajax_prepare_add_races_to_db',array($this,'ajax_prepare_add_races_to_db'));
+		add_action('wp_ajax_add_race_to_db', array($this, 'ajax_add_race_to_db'));
+	}
+
+	/**
+	 * ajax_get_race_data_non_db function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function ajax_get_race_data_non_db() {
+		if (empty($_POST['url']))
+			return false;
+
+		$this->config->url=$_POST['url']; // set url
+		$limit=false;
+
+		// set limit if passed //
+		if (!empty($_POST['limit']))
+			$limit=$_POST['limit'];
+
+		echo $this->get_race_data($limit);
+
+		wp_die();
+	}
+
+	/**
+	 * get_race_data function.
+	 *
+	 * this function does all the magic as far as parseing results and adding them to the db
+	 *
+	 * @access public
+	 * @param bool $limit (default: false)
+	 * @return void
+	 */
+	public function get_race_data($limit=false) {
+		if (!isset($this->config->url))
+			return false;
+
+		set_time_limit(0); // mex ececution time
+		$races=array();
+		$races_class_name='datatable';
+		$races_obj=new stdClass();
+		$arr=array();
+		$timeout = 5;
+		$row_count=0;
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->config->url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+
+		$html=curl_exec($ch);
+
+		curl_close($ch);
+
+		// Create a DOM parser object
+		$dom = new DOMDocument();
+
+		// Parse HTML - The @ before the method call suppresses any warnings that loadHTML might throw because of invalid HTML in the page.
+		@$dom->loadHTML($html);
+
+		//discard white space
+		@$dom->preserveWhiteSpace = false;
+
+		$finder = new DomXPath($dom);
+
+		$nodes = $finder->query("//*[contains(@class, '$races_class_name')]");
+
+		if ($nodes->length==0)
+			return '<div class="error">No rows (nodes) found. Check the url and the "races class name" ('.$races_class_name.').</div>';
+
+		$rows=$nodes->item(0)->getElementsByTagName('tr'); //get all rows from the table
+
+		// loop over the table rows
+		foreach ($rows as $row) :
+		  if ($row_count!=0) :
+		  	$races[$row_count]=new stdClass();
+
+		  	// get links //
+		  	$links = $row->getElementsByTagName('a');
+				foreach ($links as $tag) :
+					$link=$this->alter_race_link($tag->getAttribute('href'));
+				endforeach;
+
+		  	$cols = $row->getElementsByTagName('td'); 	// get each column by tag name
+			  foreach ($cols as $key => $col) :
+					if ($key==0) {
+						$races[$row_count]->date=$this->reformat_date($col->nodeValue);
+					} else if ($key==1) {
+						$races[$row_count]->event=$col->nodeValue;
+					} else if ($key==2) {
+						$races[$row_count]->nat=$col->nodeValue;
+					} else if ($key==3) {
+						$races[$row_count]->class=$col->nodeValue;
+					} else if ($key==4) {
+						$races[$row_count]->winner=$col->nodeValue;
+					}
+				endforeach;
+
+				$races[$row_count]->link=$link;
+				$races[$row_count]->season=$this->get_season_from_date($races[$row_count]->date);
+
+				// check for code in db, only get results if not in db //
+				$code=$this->build_race_code($races[$row_count]);
+			endif;
+
+			$row_count++;
+
+			if ($limit && $row_count>$limit)
+				break;
+
+		endforeach;
+
+		// setup as object //
+		foreach ($races as $key => $value) :
+			$races_obj->$key=$value;
+		endforeach;
+
+		return $this->build_default_race_table($races_obj);
+	}
+
+	/**
+	 * get_season_from_date function.
+	 *
+	 * @access protected
+	 * @param mixed $date
+	 * @return void
+	 */
+	protected function get_season_from_date($date) {
+		$season_arr=$this->build_year_arr();
+
+		foreach ($season_arr as $key => $season) :
+			if (strtotime($date) >= strtotime($season['start'])  && strtotime($date) <= strtotime($season['end']))
+				return $key;
+		endforeach;
+
+		return false;
+	}
+
+	/**
+	 * build_default_race_table function.
+	 *
+	 * @access public
+	 * @param mixed $obj
+	 * @return void
+	 */
+	public function build_default_race_table($obj) {
+		$html=null;
+
+		$html.='<form name="add-races-to-db" id="add-races-to-db" method="post">';
+			$html.='<table class="wp-list-table widefat fixed striped pages">';
+				$html.='<thead>';
+					$html.='<tr>';
+						$html.='<td id="cb" class="manage-column column-cb check-column"><input type="checkbox" id="select-all"></td>';
+						$html.='<th scope="col" class="race-date">Date</th>';
+						$html.='<th scope="col" class="race-name">Event</th>';
+						$html.='<th scope="col" class="race-nat">Nat.</th>';
+						$html.='<th scope="col" class="race-class">Class</th>';
+					$html.='</tr>';
+				$html.='</thead>';
+				$html.='<tbody id="the-list">';
+
+					foreach ($obj as $result) :
+						$disabled='';
+						$date=$result->date;
+						$code=$this->build_race_code($result->event, $result->date);
+
+						// check we have event //
+						if (!isset($result->event)) :
+							$result->event=false;
+						else :
+							$event=$result->event;
+						endif;
+
+						// disable if no event (data error) //
+						if (!$result->date || !$result->event)
+							$disabled='disabled';
+
+						if (!$result->date)
+							$date='No Date';
+
+						if (!$result->event)
+							$event='No Event';
+
+						// if we already have results, bail. there are other check later, but this is a good helper //
+						if ($this->uci_results_race_has_results($code))
+							$disabled='disabled';
+
+						$html.='<tr>';
+							$html.='<th scope="row" class="check-column"><input type="checkbox" name="races[]" value="'.base64_encode(serialize($result)).'" '.$disabled.'></th>';
+							$html.='<td class="race-date">'.$date.'</td>';
+							$html.='<td class="race-name">'.$event.'</td>';
+
+							if ($result->event) :
+								$html.='<td class="race-nat">'.$result->nat.'</td>';
+								$html.='<td class="race-class">'.$result->class.'</td>';
+							endif;
+
+						$html.='</tr>';
+					endforeach;
+
+				$html.='</tbody>';
+			$html.='</table>';
+
+			$html.='<p class="submit">';
+				$html.='<input type="button" name="button" id="add-races-curl-to-db" class="button button-primary" value="Add to DB" />';
+			$html.='</p>';
+		$html.='</form>';
+
+		return $html;
+	}
+
+	/**
+	 * build_race_code function.
+	 *
+	 * takes the race name and date to build a string which becomes our "code" to prevent dups
+	 *
+	 * @access public
+	 * @param bool $name (default: false)
+	 * @param bool $date (default: false)
+	 * @return void
+	 */
+	public function build_race_code($name=false, $date=false) {
+		if (!$date || !$name)
+			return false;
+
+		$code=$name.$date; // combine name and date
+		$code=sanitize_title_with_dashes($code); // https://codex.wordpress.org/Function_Reference/sanitize_title_with_dashes
+
+		return $code;
+	}
+
+	/**
+	 * alter_race_link function.
+	 *
+	 * @access protected
+	 * @param mixed $link
+	 * @return void
+	 */
+	protected function alter_race_link($link) {
+		$final_url='http://www.uci.infostradasports.com/asp/lib/TheASP.asp?';
+		parse_str($link,$arr);
+
+		$final_url.='PageID=19006';
+		$final_url.='&SportID='.$arr['SportID'];
+		$final_url.='&CompetitionID='.$arr['CompetitionID'];
+		$final_url.='&EditionID='.$arr['EditionID'];
+		$final_url.='&SeasonID='.$arr['SeasonID'];
+		$final_url.='&ClassID='.$arr['ClassID'];
+		$final_url.='&GenderID='.$arr['GenderID'];
+		$final_url.='&EventID='.$arr['EventID'];
+		$final_url.='&EventPhaseID='.$arr['EventPhaseID'];
+		$final_url.='&Phase1ID='.$arr['Phase1ID'];
+		$final_url.='&Phase2ID=0';
+		$final_url.='&Phase3ID=0';
+		$final_url.='&PhaseClassificationID=-1';
+		$final_url.='&Detail='.$arr['Detail'];
+		//$final_url.='&Ranking='.$arr['Ranking']; -- causes an error
+		$final_url.='&All=0';
+		$final_url.='&TaalCode=2';
+		$final_url.='&StyleID=0';
+		$final_url.='&Cache=8';
+		$final_url.='&PageNr0=-1';
+
+		return $final_url;
+	}
+
+	/**
+	 * reformat_date function.
+	 *
+	 * @access public
+	 * @param mixed $date
+	 * @return void
+	 */
+	public function reformat_date($date) {
+		$date=utf8_encode($date);
+		$date_arr=explode('Â',$date);
+
+		foreach ($date_arr as $key => $value) :
+			switch ($key) :
+				case 0:
+					$new_value=substr($value,0,-2);
+					break;
+				case 1:
+					$new_value=null;
+					$new_value=substr($value,1,-2);
+					break;
+				case 2:
+					$new_value=substr($value,1);
+					break;
+			endswitch;
+			$date_arr[$key]=$new_value;
+		endforeach;
+
+		if (count($date_arr)<3)
+			return false;
+
+		$date=$date_arr[0].' '.$date_arr[1].' '.$date_arr[2];
+
+		return $date;
+	}
+
+	/**
+	 * ajax_prepare_add_races_to_db function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function ajax_prepare_add_races_to_db() {
+		if (empty($_POST['races']))
+			return false;
+
+		$races=array();
+		foreach ($_POST['races'] as $race) :
+			$races[]=unserialize(base64_decode($race));
+		endforeach;
+
+		echo json_encode($races);
+
+		wp_die();
+	}
+
+	/**
+	 * ajax_add_race_to_db function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function ajax_add_race_to_db() {
+		if (!$_POST['race'])
+			return false;
+
+		$code=$this->build_race_code($_POST['race']);
+
+		// add to db //
+		if (!$this->check_for_dups($code)) :
+			echo $this->add_race_to_db($_POST['race']);
+		else :
+			echo '<div class="updated add-race-to-db-message">Already in db. ('.$code.')</div>';
+		endif;
+
+		wp_die();
+	}
+
+
+	/**
+	 * check_for_dups function.
+	 *
+	 * @access protected
+	 * @param mixed $code
+	 * @return void
+	 */
+	protected function check_for_dups($code) {
+		global $wpdb;
+
+		$races_in_db=$wpdb->get_results("SELECT code FROM $wpdb->ucicurl_races");
+
+		if (count($races_in_db)!=0) :
+			foreach ($races_in_db as $race) :
+				if ($race->code==$code)
+					return true;
+			endforeach;
+		endif;
+
+		return false;
+	}
+
+	/**
+	 * add_race_to_db function.
+	 *
+	 * @access public
+	 * @param mixed $race_data
+	 * @return void
+	 */
+	public function add_race_to_db($race_data) {
+		global $wpdb;
+
+		$message=null;
+
+		// convert to object //
+		if (!is_object($race_data))
+			$race_data=json_decode(json_encode($race_data),FALSE);
+
+		// build data array ..
+		$data=array(
+			'date' => $date = date('Y-m-d', strtotime($race_data->date)),
+			'event' => $race_data->event,
+			'nat' => $race_data->nat,
+			'class' => $race_data->class,
+			'winner' => $race_data->winner,
+			'season' => $race_data->season,
+			'link' => $race_data->link,
+			'code' => $this->build_race_code($race_data->event, $race_data->date),
+			'week' => $this->get_race_week($race_data->date, $race_data->season),
+		);
+
+		if (!$this->check_for_dups($data['code'])) :
+			if ($wpdb->insert($wpdb->ucicurl_races, $data)) :
+				$message='<div class="updated">Added '.$data['code'].' to database.</div>';
+				$this->add_race_results_to_db($wpdb->insert_id, $race_data->link);
+			else :
+				$message='<div class="error">Unable to insert '.$data['code'].' into the database.</div>';
+			endif;
+		else :
+			$message='<div class="updated">'.$data['code'].' is already in the database</div>';
+		endif;
+
+		return $message;
+	}
+
+	/**
+	 * get_race_week function.
+	 *
+	 * @access public
+	 * @param string $date (default: '')
+	 * @param mixed $season (default: )
+	 * @return void
+	 */
+	public function get_race_week($date='', $season='') {
+		$date=date('Y-m-d', strtotime($date));
+		$seasons=$this->build_year_arr();
+		$season_data=$this->add_weeks_to_season($seasons[$season]);
+
+		return $this->get_week_of_date($date, $season_data['weeks']);
+	}
+
+	/**
+	 * build_year_arr function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function build_year_arr() {
+		$year=date('Y');
+		$counter_year=$year-20;
+		$max_year=$year+1;
+		$season_start='Aug 30';
+		$season_end='Mar 1';
+		$season_arr=array();
+
+		while ($counter_year<=$max_year) :
+			$year=$counter_year;
+			$next_year=$counter_year+1;
+			$season=$year.'/'.$next_year;
+
+			$season_arr[$season]=array(
+				'start' => $season_start.' '.$year,
+				'end' => $season_end.' '.$next_year
+			);
+
+			$counter_year++;
+		endwhile;
+
+		return $season_arr;
+	}
+
+	/**
+	 * add_weeks_to_season function.
+	 *
+	 * @access public
+	 * @param string $season (default: '')
+	 * @return void
+	 */
+	public function add_weeks_to_season($season='') {
+		// start (first) week //
+		$start_date_arr=explode(' ', $season['start']); // get start day
+		$next_monday=strtotime('monday', mktime(0, 0, 0, date('n', strtotime($season['start'])), $start_date_arr[1], $start_date_arr[2])); // get next monday
+		$next_sunday=strtotime('sunday', $next_monday); // get next sunday
+		$first_monday=strtotime('-1 week', $next_monday); // prev monday is start
+		$first_sunday=strtotime('-1 week', $next_sunday); // prev sunday is start
+
+		// end (last) week //
+		$end_date_arr=explode(' ', $season['end']); // get end day
+		$last_monday=strtotime('monday', mktime(0, 0, 0, date('n', strtotime($season['end'])), $end_date_arr[1], $end_date_arr[2])); // get next monday
+		$last_sunday=strtotime('sunday', $last_monday); // get next sunday
+		$final_monday=strtotime('-1 week', $last_monday); // prev monday is start
+		$final_sunday=strtotime('-1 week', $last_sunday); // prev sunday is start
+
+		// build out all our weeks //
+		$monday=$first_monday;
+		$sunday=$first_sunday;
+
+		while ($monday != $final_monday) :
+	    $season['weeks'][]=array(
+	    	'start' => date('c', $monday),
+	    	'end' => date('c', $sunday)
+	    );
+
+	    $monday=strtotime('+1 week', $monday);
+	    $sunday=strtotime('+1 week', $sunday);
+		endwhile;
+
+		// append final week //
+		$season['weeks'][]=array(
+			'start' => date('c', $final_monday),
+			'end' => date('c', $final_sunday)
+		);
+
+		return $season;
+	}
+
+	/**
+	 * get_week_of_date function.
+	 *
+	 * @access public
+	 * @param string $date (default: '')
+	 * @param array $weeks (default: array())
+	 * @return void
+	 */
+	public function get_week_of_date($date='', $weeks=array()) {
+		$week_counter=1;
+
+		foreach ($weeks as $week) :
+			$week_start=strtotime($week['start']);
+			$week_end=strtotime($week['end']);
+			$date_raw=strtotime($date);
+
+			if ($date_raw>=$week_start && $date_raw<=$week_end)
+				break;
+
+			$week_counter++;
+		endforeach;
+
+		return $week_counter;
+	}
+
+}
+
+$uci_results_add_races=new UCIResultsAddRaces();
+?>
