b8cc205795dbedeb8ca91b486fce0b330c90c894
erikdmitchell <erikdmitchell@gmail.com>
2016-06-22 12:12:17 -0400

    reorder files

--- uci-curl.php
+++ uci-curl.php
@@ -12,6 +12,9 @@
 define('UCICURL_PATH', plugin_dir_path(__FILE__));
 define('UCICURL_URL', plugin_dir_url(__FILE__));
 
+include_once(UCICURL_PATH.'classes/races.php'); // our races functions
+include_once(UCICURL_PATH.'classes/riders.php'); // our riders functions
+include_once(UCICURL_PATH.'classes/uci-results-query.php'); // our query class and pagination
 include_once(UCICURL_PATH.'database.php'); // sets up our db tables
 include_once(UCICURL_PATH.'functions.php'); // all our outlying functions
 include_once(UCICURL_PATH.'init.php'); // functions to run on init
@@ -20,9 +23,6 @@ include_once(UCICURL_PATH.'admin/add-races.php'); // cURL and add races/results 
 include_once(UCICURL_PATH.'admin/rider-rankings.php'); // add and update rider rankings
 include_once(UCICURL_PATH.'admin/wp-cli.php'); // wp cli functions
 include_once(UCICURL_PATH.'lib/name-parser.php'); // a php nameparser
-include_once(UCICURL_PATH.'classes/races.php'); // our races functions
-include_once(UCICURL_PATH.'classes/riders.php'); // our riders functions
 include_once(UCICURL_PATH.'shortcode.php'); // our shortcodes
 include_once(UCICURL_PATH.'lib/flags.php'); // our flag stuff
-include_once(UCICURL_PATH.'classes/uci-results-query.php'); // our query class and pagination
 ?>
