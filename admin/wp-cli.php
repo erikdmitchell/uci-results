a435af9610bb2f84d3a1822ed486724d97f25818
erikdmitchell <erikdmitchell@gmail.com>
2016-06-28 09:25:14 -0400

    tweaked strip tags to have more raw output

--- admin/wp-cli.php
+++ admin/wp-cli.php
@@ -17,6 +17,82 @@ class UCIResultsCLI extends WP_CLI_Command {
 	}
 
 	/**
+	 * Add races (and results) to our db from a season
+	 *
+	 * ## OPTIONS
+	 *
+	 * <season>
+	 * : the season
+	 *
+	 * ## EXAMPLES
+	 *
+	 * wp uciresults add-races 2015/2016
+	 *
+	 * @subcommand add-races
+	*/
+	public function add_races($args, $assoc_args) {
+		global $wpdb, $uci_results_add_races;
+
+		$season=0;
+		$uci_urls=array(
+			'2015/2016' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=489&StartDateSort=20150830&EndDateSort=20160301&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+			'2014/2015' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=487&StartDateSort=20140830&EndDateSort=20150809&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+			'2013/2014' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=485&StartDateSort=20130907&EndDateSort=20140223&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+			'2012/2013' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=483&StartDateSort=20120908&EndDateSort=20130224&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+			'2011/2012' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=481&StartDateSort=20110910&EndDateSort=20120708&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+			'2010/2011' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=479&StartDateSort=20100911&EndDateSort=20110220&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+			'2009/2010' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=477&StartDateSort=20090913&EndDateSort=20100221&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+			'2008/2009' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=475&StartDateSort=20080914&EndDateSort=20090222&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
+		);
+
+		// set our season //
+		if (isset($args[0]) || isset($assoc_args['season'])) :
+			if (isset($args[0])) :
+				$season=$args[0];
+			elseif (isset($assoc_args['season'])) :
+				$season=$assoc_args['season'];
+			endif;
+		endif;
+
+		if (!$season || $season=='')
+			WP_CLI::error('No season found.');
+
+		// find our urls //
+		if (!isset($uci_urls[$season]) || empty($uci_urls[$season])) :
+			WP_CLI::error('No url found.');
+		else :
+			$url=$uci_urls[$season];
+		endif;
+
+		$races=$uci_results_add_races->get_race_data(false, true, $url);
+
+		if (empty($races))
+			WP_CLI::error('No races found.');
+
+		// process our races //
+		foreach ($races as $race) :
+			$code=$uci_results_add_races->build_race_code($race->event, $race->date);
+
+			if (!$code) :
+				WP_CLI::warning("Code for $race->event not crated!");
+				continue;
+			endif;
+
+			// add to db //
+			if (!$uci_results_add_races->check_for_dups($code)) :
+				$formatted_result=$uci_results_add_races->add_race_to_db($race);
+				$result=strip_tags($formatted_result);
+				WP_CLI::success($result);
+			else :
+				WP_CLI::warning("Already in db. ($code)");
+			endif;
+
+		endforeach;
+
+		WP_CLI::success("All done!");
+	}
+
+	/**
 	 * Update rider rankings in a season
 	 *
 	 * ## OPTIONS
@@ -80,89 +156,6 @@ class UCIResultsCLI extends WP_CLI_Command {
 		$week_rank_progress->finish();
 
 		WP_CLI::success('Update rider rankings complete');
-	}
-
-	/**
-	 * Add races (and results) to our db from a season
-	 *
-	 * ## OPTIONS
-	 *
-	 * <season>
-	 * : the season
-	 *
-	 * ## EXAMPLES
-	 *
-	 * wp uciresults add-races 2015/2016
-	 *
-	 * @subcommand add-races
-	*/
-	public function add_races($args, $assoc_args) {
-		global $wpdb, $uci_results_add_races;
-
-		$season=0;
-		$uci_urls=array(
-			'2015/2016' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=489&StartDateSort=20150830&EndDateSort=20160301&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-			'2014/2015' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=487&StartDateSort=20140830&EndDateSort=20150809&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-			'2013/2014' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=485&StartDateSort=20130907&EndDateSort=20140223&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-			'2012/2013' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=483&StartDateSort=20120908&EndDateSort=20130224&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-			'2011/2012' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=481&StartDateSort=20110910&EndDateSort=20120708&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-			'2010/2011' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=479&StartDateSort=20100911&EndDateSort=20110220&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-			'2009/2010' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=477&StartDateSort=20090913&EndDateSort=20100221&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-			'2008/2009' => 'http://www.uci.infostradasports.com/asp/lib/TheASP.asp?PageID=19004&TaalCode=2&StyleID=0&SportID=306&CompetitionID=-1&EditionID=-1&EventID=-1&GenderID=1&ClassID=1&EventPhaseID=0&Phase1ID=0&Phase2ID=0&CompetitionCodeInv=1&PhaseStatusCode=262280&DerivedEventPhaseID=-1&SeasonID=475&StartDateSort=20080914&EndDateSort=20090222&Detail=1&DerivedCompetitionID=-1&S00=-3&S01=2&S02=1&PageNr0=-1&Cache=8',
-		);
-
-		// set our season //
-		if (isset($args[0]) || isset($assoc_args['season'])) :
-			if (isset($args[0])) :
-				$season=$args[0];
-			elseif (isset($assoc_args['season'])) :
-				$season=$assoc_args['season'];
-			endif;
-		endif;
-
-		if (!$season || $season=='')
-			WP_CLI::error('No season found.');
-
-		// find our urls //
-		if (!isset($uci_urls[$season]) || empty($uci_urls[$season])) :
-			WP_CLI::error('No url found.');
-		else :
-			$url=$uci_urls[$season];
-		endif;
-
-		$races=$uci_results_add_races->get_race_data(false, true, $url);
-
-		if (empty($races))
-			WP_CLI::error('No races found.');
-
-		// process our races //
-		foreach ($races as $race) :
-		// date
-		// event
-		// nat
-		// class
-		// winnder
-		// link
-		// season
-			$code=$uci_results_add_races->build_race_code($race->event, $race->date);
-
-			if (!$code) :
-				WP_CLI::warning("Code for $race->event not crated!");
-				continue;
-			endif;
-
-			// add to db //
-			if (!$uci_results_add_races->check_for_dups($code)) :
-				$formatted_result=$uci_results_add_races->add_race_to_db($race);
-				$result=strip_tags($formatted_result, '<div>');
-				WP_CLI::success($result);
-			else :
-				WP_CLI::warning("Already in db. ($code)");
-			endif;
-
-		endforeach;
-
-		WP_CLI::success("All done!");
 	}
 
 	/**
