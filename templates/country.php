ab8a13f5cb823630fcac3b1f53a2794300275076
erikdmitchell <erikdmitchell@gmail.com>
2016-06-30 15:37:25 -0400

    country page is now all riders in country

--- templates/country.php
+++ templates/country.php
@@ -13,32 +13,28 @@ get_header(); ?>
 $riders=new UCI_Results_Query(array(
 	'per_page' => -1,
 	'type' => 'riders',
-	'season' => uci_results_get_default_rider_ranking_season(),
-	'rankings' => true,
 	'nat' => get_query_var('country_slug'),
+	'order_by' => 'name'
 ));
-?>
 
+// build out three column setup //
+$columns=3;
+$all_riders=$riders->posts;
+$riders_chunk=array_chunk($all_riders, ceil(count($all_riders)/$columns));
+?>
 <div class="uci-results uci-results-country">
 
 	<h1 class="page-title"><?php echo get_query_var('country_slug'); ?><span class="flag"><?php echo ucicurl_get_country_flag(get_query_var('country_slug')); ?></span><span class="season"><?php echo uci_results_get_default_rider_ranking_season(); ?></span></h1>
 
 	<?php if ($riders->have_posts()) : ?>
 		<div class="table country-riders">
-			<div class="row header">
-				<div class="rider-place">Rank</div>
-				<div class="rider-name">Name</div>
-				<div class="rider-points">Points</div>
+			<?php foreach ($riders_chunk as $key => $chunk) : ?>
+			<div class="col col-<?php echo $key; ?> columns-<?php echo $columns; ?>">
+				<?php foreach ($chunk as $arr) : ?>
+				<div class="rider-name"><a href="<?php echo uci_results_rider_url($arr->slug); ?>"><?php echo $arr->name; ?></a></div>
+				<?php endforeach; ?>
 			</div>
-
-			<?php while ( $riders->have_posts() ) : $riders->the_post(); ?>
-				<div class="row">
-					<div class="rider-place"><?php echo $uci_results_post->rank; ?></div>
-					<div class="rider-name"><a href="<?php echo uci_results_rider_url($uci_results_post->slug); ?>"><?php echo $uci_results_post->name; ?></a></div>
-					<div class="rider-points"><?php echo $uci_results_post->points; ?></div>
-				</div>
-			<?php endwhile; ?>
-
+			<?php endforeach; ?>
 		</div>
 	<?php else :?>
 		<div class="none-found">No riders from this country found.</div>
