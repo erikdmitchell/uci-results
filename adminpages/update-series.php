1e37ed5617965895347d3e84549d322fa06fbf75
erikdmitchell <erikdmitchell@gmail.com>
2016-06-30 10:43:59 -0400

    hide races in series if no race id

--- adminpages/update-series.php
+++ adminpages/update-series.php
@@ -2,16 +2,6 @@
 global $ucicurl_races, $uci_results_post;
 
 $series=$ucicurl_races->get_series_info();
-$races=new UCI_Results_Query(array(
-	'type' => 'races',
-	'meta' => array(
-		array(
-			//'table' => 'races',
-			'field' => 'series_id',
-			'value' => $series['id']
-		)
-	)
-));
 ?>
 
 <?php uci_results_admin_notices(); ?>
@@ -35,31 +25,46 @@ $races=new UCI_Results_Query(array(
 		</p>
 	</form>
 
-	<div class="races-in-series">
-		<h2>Races in Series</h2>
+	<?php if (!empty($series['id'])) : ?>
+		<?php
+		$races=new UCI_Results_Query(array(
+			'type' => 'races',
+			'meta' => array(
+				array(
+					//'table' => 'races',
+					'field' => 'series_id',
+					'value' => $series['id']
+				)
+			)
+		));
+		?>
 
-		<table class="wp-list-table widefat fixed striped races">
-			<thead>
-				<tr>
-					<th scope="col" class="race-date">Date</th>
-					<th scope="col" class="race-name">Event</th>
-					<th scope="col" class="race-nat">Nat.</th>
-					<th scope="col" class="race-class">Class</th>
-					<th scope="col" class="race-season">Season</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php if ($races->have_posts()) : while ( $races->have_posts() ) : $races->the_post(); ?>
+		<div class="races-in-series">
+			<h2>Races in Series</h2>
+
+			<table class="wp-list-table widefat fixed striped races">
+				<thead>
 					<tr>
-						<td class="race-date"><?php echo $uci_results_post->date; ?></td>
-						<td class="race-name"><a href="<?php echo admin_url('admin.php?page=uci-curl&tab=races&race_id='.$uci_results_post->id); ?>"><?php echo $uci_results_post->event; ?></a></td>
-						<td class="race-nat"><?php echo $uci_results_post->nat; ?></td>
-						<td class="race-class"><?php echo $uci_results_post->class; ?></td>
-						<td class="race-season"><?php echo $uci_results_post->season; ?></td>
+						<th scope="col" class="race-date">Date</th>
+						<th scope="col" class="race-name">Event</th>
+						<th scope="col" class="race-nat">Nat.</th>
+						<th scope="col" class="race-class">Class</th>
+						<th scope="col" class="race-season">Season</th>
 					</tr>
-				<?php endwhile; endif; ?>
-			</tbody>
-		</table>
-	</div>
+				</thead>
+				<tbody>
+					<?php if ($races->have_posts()) : while ( $races->have_posts() ) : $races->the_post(); ?>
+						<tr>
+							<td class="race-date"><?php echo $uci_results_post->date; ?></td>
+							<td class="race-name"><a href="<?php echo admin_url('admin.php?page=uci-curl&tab=races&race_id='.$uci_results_post->id); ?>"><?php echo $uci_results_post->event; ?></a></td>
+							<td class="race-nat"><?php echo $uci_results_post->nat; ?></td>
+							<td class="race-class"><?php echo $uci_results_post->class; ?></td>
+							<td class="race-season"><?php echo $uci_results_post->season; ?></td>
+						</tr>
+					<?php endwhile; endif; ?>
+				</tbody>
+			</table>
+		</div>
+	<?php endif; ?>
 
 </div>
