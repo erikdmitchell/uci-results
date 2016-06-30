abf5c27a08d6817af4979388bbe41b72ece58781
erikdmitchell <erikdmitchell@gmail.com>
2016-06-30 16:14:04 -0400

    main template built out

--- templates/main.php
+++ templates/main.php
@@ -13,50 +13,54 @@ $races=new UCI_Results_Query(array(
 ));
 ?>
 
-<div class="ucicurl-main">
-	<div class="ucicurl-riders">
+<div class="uci-results main">
+
+	<div class="uci-results-riders">
 		<h3>Rider Rankings</h3>
 
-		<div class="ucicurl-list">
-			<div class="header row">
-				<div class="rank">Rank</div>
-				<div class="rider">Rider</div>
-				<div class="nat">Nat</div>
-				<div class="points">Points</div>
+		<div class="table rider-rankings">
+			<div class="row header">
+				<div class="rider-rank">Rank</div>
+				<div class="rider-name">Rider</div>
+				<div class="rider-nat">Nat</div>
+				<div class="rider-points">Points</div>
 			</div>
 
 			<?php if ($riders->have_posts()) : while ( $riders->have_posts() ) : $riders->the_post(); ?>
 				<div class="row">
-					<div class="rank"><?php echo $uci_results_post->rank; ?></div>
-					<div class="rider"><a href="<?php uci_results_rider_url($uci_results_post->slug); ?>"><?php echo $uci_results_post->name; ?></a></div>
-					<div class="nat"><a href="<?php echo uci_results_country_url($uci_results_post->nat); ?>"><?php echo ucicurl_get_country_flag($uci_results_post->nat); ?></a></div>
-					<div class="points"><?php echo $uci_results_post->points; ?></div>
+					<div class="rider-rank"><?php echo $uci_results_post->rank; ?></div>
+					<div class="rider-name"><a href="<?php uci_results_rider_url($uci_results_post->slug); ?>"><?php echo $uci_results_post->name; ?></a></div>
+					<div class="rider-nat"><a href="<?php echo uci_results_country_url($uci_results_post->nat); ?>"><?php echo ucicurl_get_country_flag($uci_results_post->nat); ?></a></div>
+					<div class="rider-points"><?php echo $uci_results_post->points; ?></div>
 				</div>
 			<?php endwhile; endif; ?>
-
-			<a class="view-all" href="<?php uci_results_rider_rankings_url(); ?>">View All Riders &raquo;</a>
 		</div>
+
+		<a class="view-all" href="<?php uci_results_rider_rankings_url(); ?>">View All Riders &raquo;</a>
 	</div>
 
-	<div class="ucicurl-races">
+
+	<div class="uci-results-races">
 		<h3>Race Results</h3>
 
-		<div class="ucicurl-list">
-			<div class="header row">
-				<div class="name">Name</div>
-				<div class="date">Date</div>
-				<div class="nat">Nat</div>
-				<div class="class">Class</div>
+		<div class="table uci-results-races">
+			<div class="row header">
+					<div class="race-name">Name</div>
+					<div class="race-date">Date</div>
+					<div class="race-nat">Nat</div>
+					<div class="race-class">Class</div>
 			</div>
 
 			<?php if ($races->have_posts()) : while ( $races->have_posts() ) : $races->the_post(); ?>
 				<div class="row">
-					<div class="name"><a href="<?php uci_results_race_url($uci_results_post->code); ?>"><?php echo $uci_results_post->name; ?></a></div>
-					<div class="date"><?php echo $uci_results_post->date; ?></div>
-					<div class="nat"><?php echo ucicurl_get_country_flag($uci_results_post->nat); ?></div>
-					<div class="class"><?php echo $uci_results_post->class; ?></div>
+					<div class="race-name"><a href="<?php uci_results_race_url($uci_results_post->code); ?>"><?php echo $uci_results_post->name; ?></a></div>
+					<div class="race-date"><?php echo $uci_results_post->date; ?></div>
+					<div class="race-nat"><?php echo ucicurl_get_country_flag($uci_results_post->nat); ?></div>
+					<div class="race-class"><?php echo $uci_results_post->class; ?></div>
 				</div>
 			<?php endwhile; endif; ?>
+
+		</div>
 
 			<a class="view-all" href="<?php uci_results_races_url(); ?>">View All Races &raquo;</a>
 		</div>
