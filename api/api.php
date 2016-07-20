4fc3777252dce627e39a6dee3f82d1efdd3397fa
erikdmitchell <erikdmitchell@gmail.com>
2016-07-20 08:39:53 -0400

    api - basic url rewrites added

--- api/api.php
+++ api/api.php
@@ -21,11 +21,25 @@ function uci_results_api_template_loader($template) {
 }
 add_filter('template_include', 'uci_results_api_template_loader');
 
+/**
+ * uci_results_api_rewrite_rules function.
+ *
+ * @access public
+ * @return void
+ */
 function uci_results_api_rewrite_rules() {
-	add_rewrite_rule('([^/]*)/([^/]*)/?', 'index.php?page_name=api&controller=$matches[1]&action=$matches[2]', 'top');
+	add_rewrite_rule('([^/]*)/?', 'index.php?pagename=api&controller=$matches[1]', 'top');
+	add_rewrite_rule('([^/]*)/([^/]*)/?', 'index.php?pagename=api&controller=$matches[1]&action=$matches[2]', 'top');
 }
 add_action('init', 'uci_results_api_rewrite_rules', 10, 0);
 
+/**
+ * uci_results_api_register_query_vars function.
+ *
+ * @access public
+ * @param mixed $vars
+ * @return void
+ */
 function uci_results_api_register_query_vars( $vars ) {
   $vars[]='controller';
   $vars[]='action';
