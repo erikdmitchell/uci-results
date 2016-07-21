ff738efe173be26456bfc5d3855bae423671f7f0
Erik Mitchell <erikdmitchell@gmail.com>
2016-07-20 20:24:06 -0400

    single race updated with grid

--- templates/single_race.php
+++ templates/single_race.php
@@ -22,29 +22,29 @@ $race=$ucicurl_races->get_race(get_query_var('race_code'));
 	<?php else : ?>
 		<h1 class="page-title"><?php echo $race->event; ?><span class="flag"><?php echo ucicurl_get_country_flag($race->nat); ?></span></h1>
 
-		<div class="race-details">
-			<div class="race-date"><?php echo date(get_option('date_format'), strtotime($race->date)); ?></div>
-			<div class="race-class">(<?php echo $race->class; ?>)</div>
+		<div class="em-row race-details">
+			<div class="em-col-md-2 race-date"><?php echo date(get_option('date_format'), strtotime($race->date)); ?></div>
+			<div class="em-col-md-1 race-class">(<?php echo $race->class; ?>)</div>
 		</div>
 
-		<div class="table single-race">
-			<div class="row header">
-					<div class="rider-place">Place</div>
-					<div class="rider-name">Rider</div>
-					<div class="rider-points">Points</div>
-					<div class="rider-nat">Nat</div>
-					<div class="rider-age">Age</div>
-					<div class="rider-time">Time</div>
+		<div class="single-race">
+			<div class="em-row header">
+					<div class="em-col-md-1 rider-place">Place</div>
+					<div class="em-col-md-4 rider-name">Rider</div>
+					<div class="em-col-md-1 rider-points">Points</div>
+					<div class="em-col-md-1 rider-nat">Nat</div>
+					<div class="em-col-md-1 rider-age">Age</div>
+					<div class="em-col-md-2 rider-time">Time</div>
 			</div>
 
 			<?php foreach ($race->results as $result) : ?>
-				<div class="row rider-results">
-					<div class="rider-place"><?php echo $result->place; ?></div>
-					<div class="rider-name"><a href="<?php echo uci_results_rider_url($result->slug); ?>"><?php echo $result->name; ?></a></div>
-					<div class="rider-points"><?php echo $result->points; ?></div>
-					<div class="rider-nat"><a href="<?php echo uci_results_country_url($result->nat); ?>"><?php echo ucicurl_get_country_flag($result->nat); ?></a></div>
-					<div class="rider-age"><?php echo $result->age; ?></div>
-					<div class="rider-time"><?php echo $result->time; ?></div>
+				<div class="em-row rider-results">
+					<div class="em-col-md-1 rider-place"><?php echo $result->place; ?></div>
+					<div class="em-col-md-4 rider-name"><a href="<?php echo uci_results_rider_url($result->slug); ?>"><?php echo $result->name; ?></a></div>
+					<div class="em-col-md-1 rider-points"><?php echo $result->points; ?></div>
+					<div class="em-col-md-1 rider-nat"><a href="<?php echo uci_results_country_url($result->nat); ?>"><?php echo ucicurl_get_country_flag($result->nat); ?></a></div>
+					<div class="em-col-md-1 rider-age"><?php echo $result->age; ?></div>
+					<div class="em-col-md-2 rider-time"><?php echo $result->time; ?></div>
 				</div>
 			<?php endforeach; ?>
 		</div>
