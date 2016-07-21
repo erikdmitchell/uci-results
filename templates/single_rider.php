71f48904cfb88194014be8a49dfd86b1cf82ceff
Erik Mitchell <erikdmitchell@gmail.com>
2016-07-20 20:26:40 -0400

    single rider grid update

--- templates/single_rider.php
+++ templates/single_rider.php
@@ -22,24 +22,24 @@ $rider=$ucicurl_riders->get_rider(get_query_var('rider_slug'));
 		<h1 class="page-title"><?php echo ucicurl_rider_slug_to_name(get_query_var('rider_slug')); ?><span class="flag"><a href="<?php echo uci_results_country_url($rider->nat); ?>"><?php echo ucicurl_get_country_flag($rider->nat); ?></a></span></h1>
 
 		<?php if (count($rider->results)) : ?>
-			<div class="table single-rider">
-				<div class="row header">
-					<div class="race-date">Date</div>
-					<div class="race-name">Event</div>
-					<div class="rider-place">Place</div>
-					<div class="rider-points">Points</div>
-					<div class="race-class">Class</div>
-					<div class="race-season">Season</div>
+			<div class="single-rider">
+				<div class="em-row header">
+					<div class="em-col-md-2 race-date">Date</div>
+					<div class="em-col-md-5 race-name">Event</div>
+					<div class="em-col-md-1 rider-place">Place</div>
+					<div class="em-col-md-1 rider-points">Points</div>
+					<div class="em-col-md-1 race-class">Class</div>
+					<div class="em-col-md-2 race-season">Season</div>
 				</div>
 
 				<?php foreach ($rider->results as $result) : ?>
-					<div class="row">
-						<div class="race-date"><?php echo date(get_option('date_format'), strtotime($result->date)); ?></div>
-						<div class="race-name"><a href="<?php uci_results_race_url($result->race_id); ?>"><?php echo $result->event; ?></a></div>
-						<div class="rider-place"><?php echo $result->place; ?></div>
-						<div class="rider-points"><?php echo $result->par; ?></div>
-						<div class="race-class"><?php echo $result->class; ?></div>
-						<div class="race-season"><?php echo $result->season; ?></div>
+					<div class="em-row">
+						<div class="em-col-md-2 race-date"><?php echo date(get_option('date_format'), strtotime($result->date)); ?></div>
+						<div class="em-col-md-5 race-name"><a href="<?php uci_results_race_url($result->race_id); ?>"><?php echo $result->event; ?></a></div>
+						<div class="em-col-md-1 rider-place"><?php echo $result->place; ?></div>
+						<div class="em-col-md-1 rider-points"><?php echo $result->par; ?></div>
+						<div class="em-col-md-1 race-class"><?php echo $result->class; ?></div>
+						<div class="em-col-md-2 race-season"><?php echo $result->season; ?></div>
 					</div>
 				<?php endforeach; ?>
 
