7e94d42a3306acdcc9a2645002668e6b9ca9c68f
erikdmitchell <erikdmitchell@gmail.com>
2016-07-22 14:10:47 -0400

    added weekly ranks to single rider template

--- templates/single_rider.php
+++ templates/single_rider.php
@@ -12,7 +12,7 @@ get_header(); ?>
 <?php
 global $ucicurl_riders;
 
-$rider=$ucicurl_riders->get_rider(get_query_var('rider_slug'));
+$rider=$ucicurl_riders->get_rider(get_query_var('rider_slug'), true, true);
 ?>
 
 <div class="uci-results uci-results-rider">
@@ -21,8 +21,10 @@ $rider=$ucicurl_riders->get_rider(get_query_var('rider_slug'));
 
 		<h1 class="page-title"><?php echo ucicurl_rider_slug_to_name(get_query_var('rider_slug')); ?><span class="flag"><a href="<?php echo uci_results_country_url($rider->nat); ?>"><?php echo ucicurl_get_country_flag($rider->nat); ?></a></span></h1>
 
-		<?php if (count($rider->results)) : ?>
-			<div class="single-rider">
+		<?php if (isset($rider->results) && count($rider->results)) : ?>
+			<div class="single-rider-results">
+				<h3>Results</h3>
+
 				<div class="em-row header">
 					<div class="em-col-md-2 race-date">Date</div>
 					<div class="em-col-md-5 race-name">Event</div>
@@ -46,6 +48,31 @@ $rider=$ucicurl_riders->get_rider(get_query_var('rider_slug'));
 			</div>
 		<?php else :?>
 			<div class="none-found">No results.</div>
+		<?php endif; ?>
+
+		<?php if (isset($rider->rankings) && count($rider->rankings)) : ?>
+			<div class="single-rider-rankings">
+				<h3>Rankings</h3>
+
+				<div class="em-row header">
+					<div class="em-col-md-1 rank-week">Week</div>
+					<div class="em-col-md-1 rank-points">Points</div>
+					<div class="em-col-md-1 rank-place">Place</div>
+					<div class="em-col-md-2 rank-season">Season</div>
+				</div>
+
+				<?php foreach ($rider->rankings as $rank) : ?>
+					<div class="em-row">
+						<div class="em-col-md-1 rank-week"><?php echo $rank->week; ?></div>
+						<div class="em-col-md-1 rank-points"><?php echo $rank->points; ?></div>
+						<div class="em-col-md-1 rank-place"><?php echo $rank->rank; ?></div>
+						<div class="em-col-md-2 rank-season"><?php echo $rank->season; ?></div>
+					</div>
+				<?php endforeach; ?>
+
+			</div>
+		<?php else :?>
+			<div class="none-found">No rankings.</div>
 		<?php endif; ?>
 
 	<?php else : ?>
