139d8b8b2416578cf4328e4adcc72fad0271187d
erikdmitchell <erikdmitchell@gmail.com>
2016-07-27 14:36:00 -0400

    added get stats function to riders class

--- classes/riders.php
+++ classes/riders.php
@@ -2,6 +2,9 @@
 
 /**
  * UCIResultsRiders class.
+ *
+ * @since 0.1.0
+ *
  */
 class UCIResultsRiders {
 
@@ -13,16 +16,7 @@ class UCIResultsRiders {
 	 */
 	public function __construct() {}
 
-	/**
-	 * get_rider function.
-	 *
-	 * @access public
-	 * @param int $rider_id (default: 0)
-	 * @param bool $results (default: false)
-	 * @param bool $rankings (default: false)
-	 * @return void
-	 */
-	public function get_rider($rider_id=0, $results=false, $rankings=false) {
+	public function get_rider($rider_id=0, $results=false, $rankings=false, $stats=false) {
 		global $wpdb;
 
 		// if not an int, it's a slug //
@@ -51,11 +45,17 @@ class UCIResultsRiders {
 		";
 		$rider=$wpdb->get_row("SELECT * FROM $wpdb->uci_results_riders WHERE id=$rider_id");
 
+		// get results //
 		if ($results)
 			$rider->results=$wpdb->get_results($results_sql);
 
+		// get rankings //
 		if ($rankings)
 			$rider->rankings=$wpdb->get_results($rankings_sql);
+
+		// get stats //
+		if ($stats)
+			$rider->stats=$this->get_stats($rider_id);
 
 		return $rider;
 	}
@@ -87,6 +87,11 @@ class UCIResultsRiders {
 		$nats=$wpdb->get_col("SELECT DISTINCT(nat) FROM $wpdb->uci_results_riders ORDER BY nat ASC");
 
 		return $nats;
+	}
+
+	public function get_stats($rider_id=0) {
+		if (!$rider_id)
+			return false;
 	}
 
 }
