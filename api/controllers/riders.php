fcc51d2e71226ec0ddd8d420fa0012bdf364a00f
erikdmitchell <erikdmitchell@gmail.com>
2016-07-28 11:46:28 -0400

    added current rank to riders api

--- api/controllers/riders.php
+++ api/controllers/riders.php
@@ -56,5 +56,27 @@ class Riders {
 		return $rider;
 	}
 
+	/**
+	 * currentRank function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function currentRank() {
+		global $wpdb;
+
+		if (!isset($this->_params['season']) || !isset($this->_params['rider_id']))
+			return false;
+
+		// if we have week, get specific, else get last week //
+		if (isset($this->_params['week'])) :
+			$current_rank=uci_results_get_rider_rank($this->_params['rider_id'], $this->_params['season'], $this->_params['week']);
+		else :
+			$current_rank=$wpdb->get_row("SELECT * FROM $wpdb->uci_results_rider_rankings WHERE season='".$this->_params['season']."' AND rider_id=".$this->_params['rider_id']." ORDER BY week DESC LIMIT 1");
+		endif;
+
+		return $current_rank;
+	}
+
 }
 ?>
