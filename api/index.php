0db98d8e4b28189138df9ed9984b289e440f5142
erikdmitchell <erikdmitchell@gmail.com>
2016-07-28 09:24:45 -0400

    added very basic api authentication

--- api/index.php
+++ api/index.php
@@ -5,10 +5,34 @@ require_once( ABSPATH . 'wp-load.php' );
 // Define api path
 define('API_PATH', realpath(dirname(__FILE__)));
 
+// define our id-key pairs //
+$applications=array(
+	'APP001' => '28e336ac6c9423d946ba02d19c6a2632'
+);
+
 // wrap the whole thing in a try-catch block to catch any wayward exceptions! //
 try {
-    // get all of the parameters in the POST/GET request - check that query vars aren't passed either //
-    $params = $_REQUEST;
+		//get the encrypted request
+    $enc_request = $_REQUEST['enc_request'];
+
+    //get the provided app id
+    $app_id = $_REQUEST['app_id'];
+
+    //check first if the app id exists in the list of applications
+    if( !isset($applications[$app_id]) ) {
+        throw new Exception('Application does not exist!');
+    }
+
+    //decrypt the request
+    $params = json_decode(trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $applications[$app_id], base64_decode($enc_request), MCRYPT_MODE_ECB)));
+
+    //check if the request is valid by checking if it's an array and looking for the controller and action
+    if( $params == false || isset($params->controller) == false || isset($params->action) == false ) {
+        throw new Exception('Request is not valid');
+    }
+
+    //cast it into an array
+    $params = (array) $params;
 
     // get the controller and format it correctly so the first letter is always capitalized //
     if (isset($params['controller'])) :
