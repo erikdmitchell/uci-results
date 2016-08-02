ee8178cffbdc0e9aae0d5367bcff8886cbd8af6d
erikdmitchell <erikdmitchell@gmail.com>
2016-08-02 13:22:25 -0400

    layout of races template

--- templates/races.php
+++ templates/races.php
@@ -20,20 +20,20 @@ $races=new UCI_Results_Query(array(
 
 	<h1 class="page-title">Races</h1>
 
-	<div class="table uci-results-races">
-		<div class="row header">
-				<div class="race-name">Name</div>
-				<div class="race-date">Date</div>
-				<div class="race-nat">Nat</div>
-				<div class="race-class">Class</div>
+	<div class="uci-results-races">
+		<div class="em-row header">
+				<div class="em-col-md-6 race-name">Name</div>
+				<div class="em-col-md-2 race-date">Date</div>
+				<div class="em-col-md-1 race-nat">Nat</div>
+				<div class="em-col-md-1 race-class">Class</div>
 		</div>
 
 		<?php if ($races->have_posts()) : while ( $races->have_posts() ) : $races->the_post(); ?>
-			<div class="row">
-				<div class="race-name"><a href="<?php uci_results_race_url($uci_results_post->code); ?>"><?php echo $uci_results_post->name; ?></a></div>
-				<div class="race-date"><?php echo $uci_results_post->date; ?></div>
-				<div class="race-nat"><?php echo uci_results_get_country_flag($uci_results_post->nat); ?></div>
-				<div class="race-class"><?php echo $uci_results_post->class; ?></div>
+			<div class="em-row">
+				<div class="em-col-md-6 race-name"><a href="<?php uci_results_race_url($uci_results_post->code); ?>"><?php echo $uci_results_post->name; ?></a></div>
+				<div class="em-col-md-2 race-date"><?php echo $uci_results_post->date; ?></div>
+				<div class="em-col-md-1 race-nat"><?php echo uci_results_get_country_flag($uci_results_post->nat); ?></div>
+				<div class="em-col-md-1 race-class"><?php echo $uci_results_post->class; ?></div>
 			</div>
 		<?php endwhile; endif; ?>
 
