36c94040a1c25ff97acae881f342f0cf9b0fbafe
erikdmitchell <erikdmitchell@gmail.com>
2016-10-04 16:16:12 -0400

    cleaned up code

--- classes/races.php
+++ classes/races.php
@@ -306,7 +306,7 @@ class UCIcURLRaces {
 	public function weeks($season='2015/2016') {
 		global $wpdb;
 
-		$weeks=$wpdb->get_col("SELECT week FROM {$wpdb->uci_results_races} WHERE season='{$season}' GROUP BY week ORDER BY week ASC");
+		$weeks=$wpdb->get_col("SELECT week FROM $wpdb->uci_results_races WHERE season='$season' GROUP BY week ORDER BY week ASC");
 
 		return $weeks;
 	}
