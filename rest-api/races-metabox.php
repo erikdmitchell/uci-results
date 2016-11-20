2ef7f746698aaf41f373bc1cff0c6efd0ad3146b
Erik <erikdmitchell@gmail.com>
2016-11-20 10:29:32 -0500

    updated mb

--- rest-api/races-metabox.php
+++ rest-api/races-metabox.php
@@ -22,22 +22,35 @@ class UCIResultsRacesMetabox {
     public function __construct() {
         add_action('add_meta_boxes', array($this, 'add_meta_box'));
         add_action('save_post', array($this, 'save'));
+        add_action('admin_enqueue_scripts', array($this, 'admin_scripts_styles'));
+    }
+    
+    public function admin_scripts_styles($hook) {	    
+		global $wp_scripts;
+
+		// get registered script object for jquery-ui
+		$ui = $wp_scripts->query('jquery-ui-core');
+	    
+	    wp_enqueue_script('jquery-ui-datepicker');
+	    wp_enqueue_script('uci-results-admin-races-mb-script', UCI_RESULTS_API_URL.'admin/js/races-metabox.js', array('jquery-ui-datepicker'));
+
+		wp_enqueue_style('jquery-ui-smoothness', "https://ajax.googleapis.com/ajax/libs/jqueryui/{$ui->ver}/themes/smoothness/jquery-ui.min.css");
     }
  
     /**
      * Adds the meta box container.
      */
-    public function add_meta_box( $post_type ) {
+    public function add_meta_box($post_type) {
         // Limit meta box to certain post types.
         $post_types = array('races');
  
-        if ( in_array( $post_type, $post_types ) ) {
+        if (in_array($post_type, $post_types)) {
             add_meta_box(
                 'race_details',
-                __( 'Race Details', 'textdomain' ),
+                __('Race Details', 'uci-results'),
                 array($this, 'render_meta_box_content'),
                 $post_type,
-                'side',
+                'normal',
                 'default'
             );
         }
@@ -48,19 +61,15 @@ class UCIResultsRacesMetabox {
      *
      * @param int $post_id The ID of the post being saved.
      */
-    public function save( $post_id ) {
-        /*
-         * We need to verify this came from the our screen and with proper authorization,
-         * because save_post can be triggered at other times.
-         */
+    public function save($post_id) {	    
+        $prefix='_race_';
+        
         // Check if our nonce is set.
         if (!isset($_POST['uci_results_admin']))
             return $post_id;
- 
-        $nonce = $_POST['uci_results_admin'];
- 
+  
         // Verify that the nonce is valid.
-        if ( ! wp_verify_nonce( $nonce, 'update_riders_twitter_meta' ) ) {
+        if ( ! wp_verify_nonce( $_POST['uci_results_admin'], 'update_race_details' ) ) {
             return $post_id;
         }
  
@@ -68,9 +77,8 @@ class UCIResultsRacesMetabox {
          * If this is an autosave, our form has not been submitted,
          * so we don't want to do anything.
          */
-        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
             return $post_id;
-        }
  
         // Check the user's permissions.
         if ( 'page' == $_POST['post_type'] ) {
@@ -83,12 +91,20 @@ class UCIResultsRacesMetabox {
             }
         }
  
-        /* OK, it's safe for us to save the data now. */
-        // Sanitize the user input.
-        $mydata=sanitize_text_field($_POST['twitter_name']);
+        // OK, it's safe for us to save the data now. //
+        $data=$_POST['race'];
+        $data=array_map('sanitize_text_field', $data); // sanitize
+
+		// append prefix to keys //
+		foreach ($data as $key => $value) :
+			$data[$prefix.$key]=$value;
+			unset($data[$key]);
+		endforeach;
  
-        // Update the meta field.
-        update_post_meta($post_id, '_rider_twitter', $mydata);
+        // Update the meta //
+        foreach ($data as $meta_key => $meta_value) :
+        	update_post_meta($post_id, $meta_key, $meta_value);
+        endforeach;
     }
  
  
@@ -98,43 +114,55 @@ class UCIResultsRacesMetabox {
      * @param WP_Post $post The post object.
      */
     public function render_meta_box_content($post) {
-        // Add an nonce field so we can check for it later.
-        wp_nonce_field('update_riders_twitter_meta', 'uci_results_admin');
+	    $prefix='race';
+	    
+        // Add an nonce field so we can check for it later. //
+        wp_nonce_field('update_race_details', 'uci_results_admin');
  
-        // Use get_post_meta to retrieve an existing value from the database.
-        $value=get_post_meta($post->ID, '_rider_twitter',true);
+		// get values in array by matching key w/ preifx //
+		$meta=array();
+        $post_meta=get_post_meta($post->ID);
  
+        foreach ($post_meta as $key => $value) :
+			$exp_key=explode('_', $key);
+
+			if ($exp_key[1] == $prefix)
+				$meta[$exp_key[2]]=$value[0];
+		endforeach;
+
         // Display the form, using the current value.
         ?>
         
-        <div class="row">
-	        <label for="date"><?php _e('Date', 'uci-results'); ?></label>
-			<input type="text" id="date" name="date" value="<?php echo esc_attr($value); ?>" size="25" />
-        </div>
-                
-        <div class="row">
-	        <label for="date"><?php _e('Winner', 'uci-results'); ?></label>
-			<input type="text" id="date" name="date" value="<?php echo esc_attr($value); ?>" size="25" />
-        </div>
-        
-        <div class="row">
-	        <label for="date"><?php _e('Week', 'uci-results'); ?></label>
-			<input type="text" id="date" name="date" value="<?php echo esc_attr($value); ?>" size="25" />
-        </div>
-        
-        <div class="row">
-	        <label for="date"><?php _e('Link', 'uci-results'); ?></label>
-			<input type="text" id="date" name="date" value="<?php echo esc_attr($value); ?>" size="25" />
-        </div>
-        
-        <div class="row">
-	        <label for="date"><?php _e('Related Races ID', 'uci-results'); ?></label>
-			<input type="text" id="date" name="date" value="<?php echo esc_attr($value); ?>" size="25" />
-        </div>                                                        
-
-        <div class="row">
-	        <label for="date"><?php _e('Twitter', 'uci-results'); ?></label>
-			<input type="text" id="date" name="date" value="<?php echo esc_attr($value); ?>" size="25" />
+        <div class="uci-results-metabox">
+	        <div class="row">
+		        <label for="date"><?php _e('Date', 'uci-results'); ?></label>
+				<input type="text" id="date" name="race[date]" class="uci-results-datepicker date" value="<?php echo esc_attr($meta['date']); ?>" size="25" />
+	        </div>
+	                
+	        <div class="row">
+		        <label for="winner"><?php _e('Winner', 'uci-results'); ?></label>
+				<input type="text" id="winner" name="race[winner]" value="<?php echo esc_attr($meta['winner']); ?>" size="25" />
+	        </div>
+	        
+	        <div class="row">
+		        <label for="week"><?php _e('Week', 'uci-results'); ?></label>
+				<input type="text" id="week" name="race[week]" class="number" value="<?php echo esc_attr($meta['week']); ?>" size="25" />
+	        </div>
+	        
+	        <div class="row">
+		        <label for="link"><?php _e('Link', 'uci-results'); ?></label>
+				<input type="text" id="link" name="race[link]" class="code url" value="<?php echo esc_attr($meta['link']); ?>" size="25" />
+	        </div>
+	        
+	        <div class="row">
+		        <label for="related-races-id"><?php _e('Related Races ID', 'uci-results'); ?></label>
+				<input type="text" id="related-races-id" name="race[related_races_id]" class="number" value="<?php echo esc_attr($meta['related']); ?>" size="25" />
+	        </div>                                                        
+	
+	        <div class="row">
+		        <label for="twitter"><?php _e('Twitter', 'uci-results'); ?></label>
+				<input type="text" id="twitter" name="race[twitter]" value="<?php echo esc_attr($meta['twitter']); ?>" size="25" />
+	        </div>
         </div>
         
         <?php
