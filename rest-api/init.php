4bb918b4ac4ade670b8c97d032ac12226636431c
Erik <erikdmitchell@gmail.com>
2016-11-20 09:41:26 -0500

    adjusted init

--- rest-api/init.php
+++ rest-api/init.php
@@ -6,5 +6,23 @@ include_once(UCI_RESULTS_API_PATH.'riders-post-type.php');
 include_once(UCI_RESULTS_API_PATH.'races-post-type.php');
 include_once(UCI_RESULTS_API_PATH.'riders-metabox.php');
 include_once(UCI_RESULTS_API_PATH.'races-metabox.php');
-include_once(UCI_RESULTS_API_PATH.'riders-taxonomies.php');
+include_once(UCI_RESULTS_API_PATH.'taxonomies.php');
+
+// SUDO MIGRATION SCRIPT //
+/*
+global $wp_uci_curl_riders;
+
+foreach ($wp_uci_curl_riders as $rider) :
+	$arr=array(
+		'post_title' => $rider['name'],
+		'post_content' => '',
+		'post_status' => 'publish',	
+		'post_type' => 'riders',
+		'post_name' => $rider['slug'],
+	);
+	//$post_id=wp_insert_post($arr);
+	//add_post_meta($post_id, '_rider_twitter', $rider['twitter']);
+	//wp_set_object_terms($post_id, $rider['nat'], 'country', false);
+endforeach;
+*/	
 ?>
