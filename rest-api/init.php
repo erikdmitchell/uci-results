6a0ff7c02da9ccd31338d0b3b3f95945db0062f3
Erik <erikdmitchell@gmail.com>
2016-11-20 11:43:00 -0500

    moved post types to folders

--- rest-api/init.php
+++ rest-api/init.php
@@ -2,8 +2,8 @@
 define('UCI_RESULTS_API_PATH', plugin_dir_path(__FILE__));
 define('UCI_RESULTS_API_URL', plugin_dir_url(__FILE__));	
 
-include_once(UCI_RESULTS_API_PATH.'riders-post-type.php');
-include_once(UCI_RESULTS_API_PATH.'races-post-type.php');
+include_once(UCI_RESULTS_API_PATH.'post-types/riders.php');
+include_once(UCI_RESULTS_API_PATH.'post-types/races.php');
 include_once(UCI_RESULTS_API_PATH.'riders-metabox.php');
 include_once(UCI_RESULTS_API_PATH.'races-metabox.php');
 include_once(UCI_RESULTS_API_PATH.'taxonomies.php');
