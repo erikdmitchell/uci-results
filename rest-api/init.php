556dcede54cccab4dc417381a2215eaecb7651b0
Erik <erikdmitchell@gmail.com>
2016-11-22 10:17:10 -0500

    added base migration script fo db 0.2.0

--- rest-api/init.php
+++ rest-api/init.php
@@ -12,24 +12,4 @@ include_once(UCI_RESULTS_API_PATH.'functions.php');
 include_once(UCI_RESULTS_API_PATH.'api-requests.php');
 include_once(UCI_RESULTS_API_PATH.'admin/admin.php');
 include_once(UCI_RESULTS_API_PATH.'race-results.php');
-
-
-
-// SUDO MIGRATION SCRIPT //
-/*
-global $wp_uci_curl_riders;
-
-foreach ($wp_uci_curl_riders as $rider) :
-	$arr=array(
-		'post_title' => $rider['name'],
-		'post_content' => '',
-		'post_status' => 'publish',	
-		'post_type' => 'riders',
-		'post_name' => $rider['slug'],
-	);
-	//$post_id=wp_insert_post($arr);
-	//add_post_meta($post_id, '_rider_twitter', $rider['twitter']);
-	//wp_set_object_terms($post_id, $rider['nat'], 'country', false);
-endforeach;
-*/
 ?>
--- updates/upgrade-0_2_0.php
+++ updates/upgrade-0_2_0.php
@@ -14,9 +14,24 @@ function uci_results_upgrade_0_2_0() {
 	$wpdb->uci_results_series=$wpdb->prefix.'uci_curl_series';
 	$wpdb->uci_results_series_overall=$wpdb->prefix.'uci_results_series_overall';
 	
-	
-	
 	// build in hardcore migration //
+// SUDO MIGRATION SCRIPT //
+/*
+global $wp_uci_curl_riders;
+
+foreach ($wp_uci_curl_riders as $rider) :
+	$arr=array(
+		'post_title' => $rider['name'],
+		'post_content' => '',
+		'post_status' => 'publish',	
+		'post_type' => 'riders',
+		'post_name' => $rider['slug'],
+	);
+	//$post_id=wp_insert_post($arr);
+	//add_post_meta($post_id, '_rider_twitter', $rider['twitter']);
+	//wp_set_object_terms($post_id, $rider['nat'], 'country', false);
+endforeach;
+*/
 	
 	// remove tables //
 	$remove_tables_sql_query="DROP TABLE IF EXISTS $wpdb->uci_results_races, $wpdb->uci_results_results, $wpdb->uci_results_riders, $wpdb->uci_results_series;";
