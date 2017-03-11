fc62abe72f3c001e64d9842f00d3648b985c753d
Erik <erikdmitchell@gmail.com>
2017-03-11 13:14:51 -0500

    updated single rider template

--- templates/single_rider.php
+++ templates/single_rider.php
@@ -10,9 +10,9 @@
 get_header(); ?>
 
 <?php
-global $ucicurl_riders;
+global $uci_riders;
 
-$rider=$ucicurl_riders->get_rider(array(
+$rider=$uci_riders->get_rider(array(
 	'rider_id' => get_query_var('rider_slug'),
 	'results' => true,
 	'results_season' => '',
@@ -24,29 +24,29 @@ $rider=$ucicurl_riders->get_rider(array(
 <div class="uci-results uci-results-rider">
 
 	<?php if ($rider) : ?>
-		<div id="rider-<?php echo $rider->id; ?>" class="em-row rider-stats">
+		<div id="rider-<?php echo $rider->ID; ?>" class="em-row rider-stats">
 			<div class="em-col-md-4 general">
-				<h1 class="page-title"><?php echo ucicurl_rider_slug_to_name(get_query_var('rider_slug')); ?></h1>
+				<h1 class="page-title"><?php echo $rider->post_title; ?></h1>
 
-				<div class="country"><span class="label">Nationality:</span> <a href="<?php echo uci_results_country_url($rider->nat); ?>"><?php echo uci_results_get_country_flag($rider->nat); ?></a></div>
-				<div class="rank"><span class="label">Ranking:</span> <?php echo $rider->rank->rank; ?></div>
+				<div class="country"><span class="">Nationality:</span> <a href="<?php echo uci_results_country_url($rider->nat); ?>"><?php echo uci_results_get_country_flag($rider->nat); ?></a></div>
+				<div class="rank"><span class="">Ranking:</span> <?php echo $rider->rank->rank; ?></div>
 			</div>
 			<div class="em-col-md-4 championships">
 				<h4>Championships</h4>
 
-				<div class="world-titles"><span class="label">World Titles:</span> <?php uci_results_display_total($rider->stats->world_champs); ?></div>
-				<div class="world-cup-titles"><span class="label">World Cup Titles:</span> <?php uci_results_display_total($rider->stats->world_cup_titles); ?></div>
-				<div class="superprestige-titles"><span class="label">Superprestige Titles:</span> <?php uci_results_display_total($rider->stats->superprestige_titles); ?></div>
-				<div class="bpost-bank-titles"><span class="label">Gva/BPost Bank Titles:</span> <?php uci_results_display_total($rider->stats->gva_bpost_bank_titles); ?></div>
+				<div class="world-titles"><span class="">World Titles:</span> <?php uci_results_display_total($rider->stats->world_champs); ?></div>
+				<div class="world-cup-titles"><span class="">World Cup Titles:</span> <?php uci_results_display_total($rider->stats->world_cup_titles); ?></div>
+				<div class="superprestige-titles"><span class="">Superprestige Titles:</span> <?php uci_results_display_total($rider->stats->superprestige_titles); ?></div>
+				<div class="bpost-bank-titles"><span class="">Gva/BPost Bank Titles:</span> <?php uci_results_display_total($rider->stats->gva_bpost_bank_titles); ?></div>
 			</div>
 			<div class="em-col-md-4 top-results">
 				<h4>Top Results</h4>
 
-				<div class="wins"><span class="label">Wins:</span> <?php uci_results_display_total($rider->stats->wins); ?></div>
-				<div class="podiums"><span class="label">Podiums:</span> <?php uci_results_display_total($rider->stats->podiums); ?></div>
-				<div class="world-cup-wins"><span class="label">World Cup Wins:</span> <?php uci_results_display_total($rider->stats->world_cup_wins); ?></div>
-				<div class="superprestige-wins"><span class="label">Superprestige Wins:</span> <?php uci_results_display_total($rider->stats->superprestige_wins); ?></div>
-				<div class="bpost-bank-wins"><span class="label">GvA/BPost Bank Wins:</span> <?php uci_results_display_total($rider->stats->gva_bpost_bank_wins); ?></div>
+				<div class="wins"><span class="">Wins:</span> <?php uci_results_display_total($rider->stats->wins); ?></div>
+				<div class="podiums"><span class="">Podiums:</span> <?php uci_results_display_total($rider->stats->podiums); ?></div>
+				<div class="world-cup-wins"><span class="">World Cup Wins:</span> <?php uci_results_display_total($rider->stats->world_cup_wins); ?></div>
+				<div class="superprestige-wins"><span class="">Superprestige Wins:</span> <?php uci_results_display_total($rider->stats->superprestige_wins); ?></div>
+				<div class="bpost-bank-wins"><span class="">GvA/BPost Bank Wins:</span> <?php uci_results_display_total($rider->stats->gva_bpost_bank_wins); ?></div>
 			</div>
 		</div>
 
@@ -65,12 +65,12 @@ $rider=$ucicurl_riders->get_rider(array(
 
 				<?php foreach ($rider->results as $result) : ?>
 					<div class="em-row">
-						<div class="em-col-md-2 race-date"><?php echo date(get_option('date_format'), strtotime($result->date)); ?></div>
-						<div class="em-col-md-5 race-name"><a href="<?php uci_results_race_url($result->race_id); ?>"><?php echo $result->event; ?></a></div>
-						<div class="em-col-md-1 rider-place"><?php echo $result->place; ?></div>
-						<div class="em-col-md-1 rider-points"><?php echo $result->par; ?></div>
-						<div class="em-col-md-1 race-class"><?php echo $result->class; ?></div>
-						<div class="em-col-md-2 race-season"><?php echo $result->season; ?></div>
+						<div class="em-col-md-2 race-date"><?php echo date(get_option('date_format'), strtotime($result['race_date'])); ?></div>
+						<div class="em-col-md-5 race-name"><a href="<?php uci_results_race_url($result['race_id']); ?>"><?php echo $result['race_name']; ?></a></div>
+						<div class="em-col-md-1 rider-place"><?php echo $result['place']; ?></div>
+						<div class="em-col-md-1 rider-points"><?php echo $result['par']; ?></div>
+						<div class="em-col-md-1 race-class"><?php echo $result['race_class']; ?></div>
+						<div class="em-col-md-2 race-season"><?php echo $result['race_season']; ?></div>
 					</div>
 				<?php endforeach; ?>
 
