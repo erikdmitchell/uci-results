1bf1c22486e84c84bdbb8cddd93e31be87a09134
Erik <erikdmitchell@gmail.com>
2017-03-18 14:50:07 -0400

    added race date to race rest api

--- rest-api/filters.php
+++ rest-api/filters.php
@@ -4,6 +4,7 @@ function rest_prepare_races($response, $post, $request) {
 	$response->data['race_class']['rendered']=uci_get_first_term($post->ID, 'race_class');
 	$response->data['season']['rendered']=uci_get_first_term($post->ID, 'season');
 	$response->data['series']['rendered']=uci_get_first_term($post->ID, 'series');
+	$response->data['race_date']=get_post_meta($post->ID, '_race_date', true);
 	$response->data['results']=uci_results_get_race_results($post->ID, 'object');
 			
 	//print_r($response);
