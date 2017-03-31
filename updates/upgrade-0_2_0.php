d413659faacc73229cd459b6299b81b712775b1a
Erik <erikdmitchell@gmail.com>
2017-03-31 09:55:17 -0400

    reverted upgrade 0.2.0 to master upgrade

--- updates/upgrade-0_2_0.php
+++ updates/upgrade-0_2_0.php
@@ -1,15 +1,12 @@
 <?php
-// this is a major migration, so we wrote a script for it (seperate admin page) //
 
 /**
- * upgrade_0_2_0_db function.
+ * uci_results_upgrade_0_2_0 function.
  * 
  * @access public
  * @return void
  */
-function upgrade_0_2_0_db() {
-	require_once(ABSPATH.'wp-admin/includes/upgrade.php');
-
+function uci_results_upgrade_0_2_0() {
 	global $wpdb;
 	
 	$db_version='0.2.0';
@@ -26,37 +23,14 @@ function upgrade_0_2_0_db() {
 		),
 	);
 
-	$wpdb->hide_errors();
-	$wpdb->uci_results_season_weeks=$wpdb->prefix.'uci_results_season_weeks';
-	$charset=$wpdb->get_charset_collate();
-
-	$sql_season_weeks="
-		CREATE TABLE $wpdb->uci_results_season_weeks (
-		  	id bigint(20) NOT NULL AUTO_INCREMENT,
-		  	term_id bigint(20) NOT NULL DEFAULT '0',
-		  	week bigint(20) NOT NULL DEFAULT '0',
-			start date NOT NULL,
-			end date NOT NULL,
-			PRIMARY KEY (`id`)
-		) $charset;
-	";
-
-	dbDelta(array(
-		$sql_season_weeks,
-	));
+	// cycle through and setup queries //
+	foreach ($indexs as $table => $index_cols) :
+		foreach ($index_cols as $col) :
+			if (!uci_results_index_exists($table, $col))
+				uci_results_create_index($table, $col);
+		endforeach;
+	endforeach;
+	
+	return $db_version;
 }
-
-/**
- * uci_results_upgrade_0_2_0_notice function.
- * 
- * @access public
- * @return void
- */
-function uci_results_upgrade_0_2_0_notice() {
-	$class = 'migration-0_2_0 notice notice-warning';
-	$message = __('The UCI Results Plugin requires a major database upgrade. <a href="'.admin_url('?page=uci-results&subpage=migration&version=0_2_0').'">Click here</a>', 'uci-results');
-
-	printf('<div class="%1$s"><p>%2$s</p></div>', $class, $message); 
-}
-add_action('admin_notices', 'uci_results_upgrade_0_2_0_notice');
 ?>
