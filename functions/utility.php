2e7232702a1828c0ed9a600be908b38053d25761
Erik <erikdmitchell@gmail.com>
2017-03-31 10:41:05 -0400

    added uci_results_format_size

--- functions/utility.php
+++ functions/utility.php
@@ -159,4 +159,21 @@ function array_to_object($array) {
     }
     return $object;
 }
+
+/**
+ * uci_results_format_size function.
+ * 
+ * @access public
+ * @param string $size (default: '')
+ * @return void
+ */
+function uci_results_format_size($size='') {
+	$sizes = array(" Bytes", " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
+	
+    if ($size == 0) : 
+	    return('n/a');
+	else :
+      	return (round($size/pow(1024, ($i = floor(log($size, 1024)))), 2) . $sizes[$i]);
+    endif;
+}
 ?>
