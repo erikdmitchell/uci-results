059cd87c2bbdc839283eccb8a9d68e4119a6578b
Erik <erikdmitchell@gmail.com>
2017-04-19 10:21:44 -0400

    testing filters

--- rest-api/filters.php
+++ rest-api/filters.php
@@ -6,10 +6,23 @@ function rest_prepare_races($response, $post, $request) {
 	$response->data['series']['rendered']=uci_get_first_term($post->ID, 'series');
 	$response->data['race_date']=get_post_meta($post->ID, '_race_date', true);
 	$response->data['results']=uci_results_get_race_results($post->ID, 'object');
-			
-	//print_r($response);
 	
 	return $response;
 }
 add_filter('rest_prepare_races', 'rest_prepare_races', 10, 3);
+
+function rest_prepare_riders($response, $post, $request) {
+print_r($request);
+/*
+	$response->data['country']['rendered']=uci_get_first_term($post->ID, 'country');
+	$response->data['race_class']['rendered']=uci_get_first_term($post->ID, 'race_class');
+	$response->data['season']['rendered']=uci_get_first_term($post->ID, 'season');
+	$response->data['series']['rendered']=uci_get_first_term($post->ID, 'series');
+	$response->data['race_date']=get_post_meta($post->ID, '_race_date', true);
+	$response->data['results']=uci_results_get_race_results($post->ID, 'object');
+*/
+	
+	return $response;
+}
+add_filter('rest_prepare_riders', 'rest_prepare_riders', 10, 3);
 ?>
