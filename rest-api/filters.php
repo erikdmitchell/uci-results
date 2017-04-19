f431260b917bb574af5759d66cec8f58f048e285
Erik <erikdmitchell@gmail.com>
2017-04-19 11:14:28 -0400

    added riders filter

--- rest-api/filters.php
+++ rest-api/filters.php
@@ -11,6 +11,22 @@ function rest_prepare_races($response, $post, $request) {
 }
 add_filter('rest_prepare_races', 'rest_prepare_races', 10, 3);
 
+function rest_prepare_riders($response, $post, $request) {
+	if ($request['results'])
+		$response->data['results']='abc';
+/*
+	$response->data['country']['rendered']=uci_get_first_term($post->ID, 'country');
+	$response->data['race_class']['rendered']=uci_get_first_term($post->ID, 'race_class');
+	$response->data['season']['rendered']=uci_get_first_term($post->ID, 'season');
+	$response->data['series']['rendered']=uci_get_first_term($post->ID, 'series');
+	$response->data['race_date']=get_post_meta($post->ID, '_race_date', true);
+	$response->data['results']=uci_results_get_race_results($post->ID, 'object');
+*/
+	
+	return $response;
+}
+add_filter('rest_prepare_riders', 'rest_prepare_riders', 10, 3);
+
 function uci_posts_item_args($item_args, $post_type, $schema) {	
 	switch ($post_type) :
 		case 'riders' :
